b1d3e72712f30a6fdec298c9566aa5b521088259
Игорь Веремиенко В <i.veremienko@bars-open.ru>
2024-07-25 18:04:14 +0500

    data_structure

--- block_1/data_structure/task_1/implementation.py
+++ block_1/data_structure/task_1/implementation.py
@@ -3,7 +3,11 @@ class Tuple:
     Создает неизменяемый объект с упорядоченной структурой и методами count и index.
     При создании принимается последовательность объектов.
     """
-
+    vals = tuple()
+    def __init__(self, *args):
+        self.vals = args
+        
+        
     def count(self, value) -> int:
         """
         Возвращает количество появлений value в объекте.
@@ -11,6 +15,11 @@ class Tuple:
         Args:
             value: количество вхождений в объекте
         """
+        val_count = 0
+        for i in range(len(self.vals)):
+            if self.vals[i] == value:
+                val_count += 1
+        return val_count
         raise NotImplementedError
 
     def index(self, value) -> int:
@@ -20,4 +29,16 @@ class Tuple:
         Args:
             value: индекс искомого элемента
         """
-        raise NotImplementedError
+        for i in range(len(self.vals)):
+            if self.vals[i] == value:
+                return i
+        raise ValueError
+
+    def __getitem__(self, item):
+        return self.vals[item]
+
+user_tuple = Tuple(1,2,3,3)
+print(user_tuple[0])
+print(user_tuple.count(3))
+print(user_tuple.index(3))
+
--- block_1/data_structure/task_3/implementation.py
+++ block_1/data_structure/task_3/implementation.py
@@ -1,5 +1,14 @@
+import copy
+
 def copy_dict(origin_dict: dict) -> dict:
-    """
-    Функция возвращает копию словаря.
-    """
-    raise NotImplementedError
+    new_dict = dict()
+    for key, val in origin_dict.items():
+        # print(f'{key}, {val}')
+        if (isinstance(val,dict)):
+            new_dict[key] = copy_dict(val)
+        elif (isinstance(val,list)):
+            new_dict[key] = copy.deepcopy(val)
+        else:
+            new_dict[key] = val
+    return new_dict
+    # raise NotImplementedError
