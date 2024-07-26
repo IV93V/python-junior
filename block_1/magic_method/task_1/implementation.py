7699d6abb1f0ccb613803dff0ba4f4a07b355aca
Игорь Веремиенко В <i.veremienko@bars-open.ru>
2024-07-26 19:51:16 +0500

    magic_methods

--- block_1/magic_method/task_1/implementation.py
+++ block_1/magic_method/task_1/implementation.py
@@ -1,24 +1,48 @@
 class Multiplier:
-    def __init__(self):
-        pass
-
+    val = int
+    def __init__(self,value):
+        self.val = value
+    
+    def __add__(self,obj):
+        if (isinstance(obj, Multiplier)):
+            return Multiplier(self.val + obj.val)
+        else:
+            return Multiplier(self.val + obj)
+        
+    def __sub__(self,obj):    
+        if (isinstance(obj, Multiplier)):
+            return Multiplier(self.val - obj.val)
+        else:
+            return Multiplier(self.val - obj)
+    
+    def __mul__(self,obj):
+        if (isinstance(obj, Multiplier)):
+            return Multiplier(self.val * obj.val)
+        else:
+            return Multiplier(self.val * obj)
+    
+    def __truediv__(self,obj):
+        if (isinstance(obj, Multiplier)):
+            return Multiplier(self.val / obj.val)
+        else:
+            return Multiplier(self.val / obj)
+        
+    def get_value(self):
+        return int(self.val)
 
 class Hundred(Multiplier):
     """Множитель на 100"""
-    def __init__(self):
-        pass
+    def __init__(self,value):
+       self.val = value*100
 
 
 class Thousand(Multiplier):
     """Множитель на 1 000"""
-    def __init__(self):
-        pass
+    def __init__(self,value):
+        self.val = value*1000
 
 
 class Million(Multiplier):
     """Множитель на 1 000 000"""
-    def __init__(self):
-        pass
-
-
-
+    def __init__(self,value):
+        self.val = value*1000000
--- block_1/magic_method/task_2/implementation.py
+++ block_1/magic_method/task_2/implementation.py
@@ -1,8 +1,31 @@
 class MathClock:
+    
     def __init__(self):
-        pass
-
+        self.t = int
+        self.h = 0
+        self.m = 0
+        
+    def __add__(self,value):
+        self.t = self.h*60 + self.m
+        self.m = (self.t+value)%60
+        self.h = (self.t+value)//60
+        
+    def __sub__(self,value):
+        self.t = self.h*60 + self.m
+        self.m = (self.t-value)%60
+        self.h = (self.t-value)//60
+        
+    def __mul__(self,value):
+        self.m = self.m
+        self.h = (self.h+value)%24
+    
+    def __truediv__(self,value):
+        self.m = self.m
+        self.h = (self.h-value)%24
     def get_time(self):
-        return 0
+        return "{:02d}:{:02d}".format(self.h, self.m)
 
 
+time_obj = MathClock()
+time_obj * 9
+print(time_obj.get_time())         
