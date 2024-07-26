78ccb82576cd362e59154acc44b2931fd989f4e6
Игорь Веремиенко В <i.veremienko@bars-open.ru>
2024-07-26 08:35:33 +0500

    comprehensions

--- block_1/comprehensions/task_1/implementation.py
+++ block_1/comprehensions/task_1/implementation.py
@@ -7,8 +7,6 @@ def flatten_list(matrix: list) -> list:
 
     Returns:
         линейный список
-    """
-    
+    """    
     return [matrix[i][j] for i in range(len(matrix)) for j in (range(len(matrix[i])))]
-    
     raise NotImplementedError
